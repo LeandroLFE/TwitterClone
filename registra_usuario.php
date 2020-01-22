0f3133553d9be0381915f3b0a2378aadb82a4298
LeandroLFE <lfe.leandro@gmail.com>
2020-01-22 13:49:25 -0300

    Solved bugs and implemented form validations to DB

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,6 +1,6 @@
 <?php
 
-    require('db.php');
+    require_once('db.php');
 
     $usuario = $_POST['usuario']??null;
 
---
+++ validar_acesso.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once('db.php');
+
+    $usuario = $_POST['usuario']??null;
+    $senha = $_POST['senha']??null;
+
+    $obJDb = new db();
+    $link = $obJDb->conecta_mysql();
+
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
+    $resp = mysqli_query($link, $sql);
+    $respAssoc = mysqli_fetch_assoc($resp);
+
+    if(!$resp){
+        echo "Erro na consulta ao Banco de Dados, favor contactar o administrador do site";
+    } else {
+        if(isset($respAssoc['usuario'])
+        && isset($respAssoc['senha']) 
+        && password_verify($senha, $respAssoc['senha'])){
+            echo "usuario validado";
+        } else{
+            header("Location: index.php?erro=1");
+        }  
+    } 
